5c3ff8864c4c0166a8f2f6cde087df61d900c229
Mohammed Alsobay <mosobay@mit.edu>
2024-03-05 23:22:24 -0500

    change icons for students to images

--- client/game/Student.jsx
+++ client/game/Student.jsx
@@ -80,16 +80,12 @@ export default class Student extends React.Component {
         className="student"
         style={cursorStyle}
       >
-        {/* <span className="icon bp3-icon-standard bp3-icon-person" /> */}
-        <span className="icon">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
-            <path
-              style={style}
-              d="M96 0c35.346 0 64 28.654 64 64s-28.654 64-64 64-64-28.654-64-64S60.654 0 96 0m48 144h-11.36c-22.711 10.443-49.59 10.894-73.28 0H48c-26.51 0-48 21.49-48 48v136c0 13.255 10.745 24 24 24h16v136c0 13.255 10.745 24 24 24h64c13.255 0 24-10.745 24-24V352h16c13.255 0 24-10.745 24-24V192c0-26.51-21.49-48-48-48z"
-            />
-          </svg>
-        </span>
-        <span className="letter">{student}</span>
+        <img
+          src="resources/icons/arab.png" // Replace with the actual path to the image file
+          alt={student}
+          style={{width: "60px", padding: "10px"}}
+        />
+        {/* <span className="letter">{student}</span> */}
       </div>
     );
   }
